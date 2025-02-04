5c0dd6ace2f4825c71296645cf6c5495df896db0
gabrielafjesuino <167575361+gabrielafjesuino@users.noreply.github.com>
2025-02-04 17:13:06 -0300

    Add files via upload

--- view.py
+++ view.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from main import app, con
 
 @app.route('/livro', methods=['GET'])
 def livro():
     cur = con.cursor()
-    cur.execute("SELECT id_livro, titulo, autor, ano_publicacao FROM livro")
+    cur.execute("SELECT id_livro, titulo, autor, ano_publicacao FROM livros")
     livros = cur.fetchall()
     livros_dic = []
     for livro in livros:
@@ -14,4 +14,62 @@ def livro():
             'autor': livro[2],
             'ano_publicacao': livro[3]
         })
-        return jsonify(mensagem='Lista de livros', livros=livros_dic)
+    return jsonify(mensagem='Lista de Livros', livros=livros_dic)
+
+@app.route('/livro', methods=['POST'])
+def livro_post():
+    data = request.get_json()
+    titulo = data.get('titulo')
+    autor = data.get('autor')
+    ano_publicacao = data.get('ano_publicacao')
+
+    cursor = con.cursor()
+
+    cursor.execute("SELECT 1 FROM LIVROS WHERE TITULO = ?", (titulo,))
+
+    if cursor.fetchone():
+        return jsonify('Livro já cadastrado!')
+
+    cursor.execute('INSERT INTO LIVROS (TITULO, AUTOR, ANO_PUBLICACAO) VALUES (?,?,?)', (titulo, autor, ano_publicacao))
+
+    con.commit()
+    cursor.close()
+
+    return jsonify({
+        'message':'Livro cadastrado com sucesso!',
+        'livro': {
+            'titulo': titulo,
+            'autor': autor,
+            'ano_publicacao': ano_publicacao
+        }
+    })
+
+@app.route('/livro/<int:id>', methods=['PUT'])
+def livro_put(id):
+    cursor = con.cursor()
+    cursor.execute("select id_livro, titulo, autor, ano_publicacao from livros WHERE id_livro = ?", (id,))
+    livro_data = cursor.fetchone()
+
+    if not livro_data:
+        cursor.close()
+        return jsonify({"error":"Livro não foi encontrado!"}),404
+
+    data = request.get_json()
+    titulo = data.get('titulo')
+    autor = data.get('autor')
+    ano_publicacao = data.get('ano_publicacao')
+
+    cursor.execute("update livros set titulo = ?, autor = ?, ano_publicacao = ? where id_livro = ?",
+                   (titulo, autor, ano_publicacao, id))
+
+    con.commit()
+    cursor.close()
+
+    return jsonify({
+        'message':'Livro atualizado com sucesso!',
+        'livro': {
+            'titulo': titulo,
+            'autor': autor,
+            'ano_publicacao': ano_publicacao
+        }
+    })
